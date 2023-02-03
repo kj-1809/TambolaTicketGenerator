80c1131928f9c7e9fb481253032844adba182e61
Krishna Jindal <rajeshkrjindal@Rajeshs-MacBook-Air.local>
2023-02-03 19:56:28 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import Ticket from "./components/Ticket"
+import "./App.css"
 const App = () => (
-  <div>
+  <div className = "container">
+    {
+      Array.from({length : 100}).map((ele) => {
+        return <Ticket />;
+      })
+    }
   </div>
 );
 
